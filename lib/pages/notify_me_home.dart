03bc9167fa77e71087319c968123beb665fd8abd
onatcipli <onatcipli@outlook.com>
2019-12-22 14:14:16 +0300

    update design

--- lib/pages/notify_me_home.dart
+++ lib/pages/notify_me_home.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:notify_me/widgets/build_favorites.dart';
+import 'package:notify_me/widgets/build_regular_notify_cards.dart';
 
 class NotifyMeHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: BuildFavorites(),
+        child: Column(
+          children: <Widget>[
+            BuildFavorites(),
+            BuildRegularNotifyCards(),
+          ],
+        ),
       ),
     );
   }
