860bda3a15644a40ebbbf6c441611b5e2c155906
onatcipli <onatcipli@outlook.com>
2019-12-22 14:46:39 +0300

    update design

---
+++ lib/pages/notify_full_page.dart
@@ -0,0 +1,161 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:notify_me/models/notify_card_model.dart';
+import 'package:notify_me/widgets/notify_favorite_card.dart';
+
+class NotifyFullPage extends StatelessWidget {
+  final NotifyCardModel notifyCardModel;
+
+  const NotifyFullPage({Key key, this.notifyCardModel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: gradientBeginColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              CachedNetworkImage(
+                imageUrl: notifyCardModel.backgroundImageUrl,
+              ),
+              SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Container(
+                        width: 48,
+                        height: 48,
+                        child: CircleAvatar(
+                          backgroundImage:
+                              NetworkImage(notifyCardModel.profileImageUrl),
+                        ),
+                      ),
+                      IconButton(
+                        color: Colors.white,
+                        icon: Icon(Icons.close),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Text(
+                          notifyCardModel.title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          '~' + notifyCardModel.subTitle,
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.9),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          notifyCardModel.time,
+                          style: TextStyle(color: Colors.yellow),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          width: 64,
+                          height: 64,
+                          child: CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(notifyCardModel.profileImageUrl),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          notifyCardModel.name,
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.bookmark_border),
+                    color: Colors.white,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.refresh),
+                    color: Colors.white,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.favorite_border),
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/*
+
+Align(
+alignment: Alignment.bottomCenter,
+child: Container(
+height: 50,
+width: double.infinity,
+decoration: BoxDecoration(
+gradient: LinearGradient(
+begin: Alignment.bottomCenter,
+end: Alignment.topCenter,
+colors: <Color>[
+gradientBeginColor,
+gradientBeginColor,
+Colors.transparent
+],
+),
+),
+child: Text('asdasdasdasdasd'),
+),
+)*/
--- lib/widgets/notify_favorite_card.dart
+++ lib/widgets/notify_favorite_card.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:notify_me/models/notify_card_model.dart';
+import 'package:notify_me/pages/notify_full_page.dart';
 
 const Color gradientBeginColor = Color(0xff222831);
 
@@ -11,114 +12,124 @@ class NotifyFavoriteCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Stack(
-        fit: StackFit.passthrough,
-        children: <Widget>[
-          CachedNetworkImage(
-            fit: BoxFit.fill,
-            imageUrl: notifyCardModel.backgroundImageUrl,
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: FittedBox(
-              fit: BoxFit.fitWidth,
-              child: Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: <Color>[
-                    gradientBeginColor,
-                    gradientBeginColor,
-                    Colors.transparent
-                  ],
-                )),
-                height: 150,
-                width: 290,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                notifyCardModel.title,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                '~' + notifyCardModel.subTitle,
-                                style: TextStyle(
-                                  color: Colors.white.withOpacity(0.9),
-                                ),
-                              ),
-                              Text(
-                                notifyCardModel.time,
-                                style: TextStyle(color: Colors.yellow),
-                              ),
-                            ],
-                          ),
-                          Container(
-                            width: 64,
-                            height: 64,
-                            child: CircleAvatar(
-                              backgroundImage:
-                                  NetworkImage(notifyCardModel.profileImageUrl),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.bookmark_border),
-                            color: Colors.white,
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.refresh),
-                            color: Colors.white,
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.favorite_border),
-                            color: Colors.white,
-                          ),
-                        ],
-                      )
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (BuildContext context) {
+          return NotifyFullPage(
+            notifyCardModel: notifyCardModel,
+          );
+        }));
+      },
+      child: Card(
+        elevation: 5,
+        child: Stack(
+          fit: StackFit.passthrough,
+          children: <Widget>[
+            CachedNetworkImage(
+              fit: BoxFit.fill,
+              imageUrl: notifyCardModel.backgroundImageUrl,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: <Color>[
+                      gradientBeginColor,
+                      gradientBeginColor,
+                      Colors.transparent
                     ],
+                  )),
+                  height: 150,
+                  width: 290,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  notifyCardModel.title,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  '~' + notifyCardModel.subTitle,
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(0.9),
+                                  ),
+                                ),
+                                Text(
+                                  notifyCardModel.time,
+                                  style: TextStyle(color: Colors.yellow),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              width: 64,
+                              height: 64,
+                              child: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    notifyCardModel.profileImageUrl),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.bookmark_border),
+                              color: Colors.white,
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.refresh),
+                              color: Colors.white,
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.favorite_border),
+                              color: Colors.white,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            top: 0,
-            right: 0,
-            child: IconButton(
-              icon: Icon(Icons.more_vert),
-              color: Colors.white,
-              onPressed: () {},
-            ),
-          )
-        ],
+            Positioned(
+              top: 0,
+              right: 0,
+              child: IconButton(
+                icon: Icon(Icons.more_vert),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
