c6dafc14774e1e53819b303c7252719751383845
onatcipli <onatcipli@outlook.com>
2020-01-01 11:16:32 +0300

    bottom navigator added

--- lib/pages/notify_me_home.dart
+++ lib/pages/notify_me_home.dart
@@ -1,19 +1,96 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:notify_me/widgets/build_favorites.dart';
 import 'package:notify_me/widgets/build_regular_notify_cards.dart';
 
-class NotifyMeHome extends StatelessWidget {
+class NotifyMeHome extends StatefulWidget {
+  @override
+  _NotifyMeHomeState createState() => _NotifyMeHomeState();
+}
+
+class _NotifyMeHomeState extends State<NotifyMeHome> {
+  int _selectedIndex = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _selectedIndex,
+        showElevation: true, // use this to remove appBar's elevation
+        onItemSelected: (index) => setState(() {
+          _selectedIndex = index;
+          _pageController.animateToPage(index,
+              duration: Duration(milliseconds: 300), curve: Curves.ease);
+        }),
+        items: [
+          BottomNavyBarItem(
+            icon: Icon(Icons.apps),
+            title: Text('Home'),
+            activeColor: Colors.red,
+          ),
+          BottomNavyBarItem(
+              icon: Icon(Icons.people),
+              title: Text('Users'),
+              activeColor: Colors.purpleAccent),
+          BottomNavyBarItem(
+              icon: Icon(Icons.message),
+              title: Text('Messages'),
+              activeColor: Colors.pink),
+          BottomNavyBarItem(
+              icon: Icon(Icons.settings),
+              title: Text('Settings'),
+              activeColor: Colors.blue),
+        ],
+      ),
       body: SafeArea(
-        child: Column(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _selectedIndex = index);
+          },
           children: <Widget>[
-            BuildFavorites(),
-            BuildRegularNotifyCards(),
+            BuildExplorePage(),
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.green,
+            ),
+            Container(
+              color: Colors.blue,
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class BuildExplorePage extends StatelessWidget {
+  const BuildExplorePage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        BuildFavorites(),
+        BuildRegularNotifyCards(),
+      ],
     );
   }
 }
