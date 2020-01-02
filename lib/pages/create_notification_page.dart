bb525d8fb904b70411183f2516c46b6558082d3c
Mustafa Berkay <Mustafa.Berkay@logo.com.tr>
2020-01-02 22:58:42 +0300

    Create Page

--- lib/pages/create_notification_page.dart
+++ lib/pages/create_notification_page.dart
@@ -27,33 +27,54 @@ class _CreateNotificationState extends State<CreateNotification> {
             style: TextStyle(fontSize: 24),
           ),
         ),
-        Card(),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(18, 0, 0, 0),
-                child: Text(currentValue.toInt().toString()),
-              ),
-              Expanded(
-                child: Slider(
-                  onChanged: (double val) {
-                    setState(() {
-                      currentValue = val;
-                    });
-                  },
-                  value: currentValue,
-                  max: maxValue,
-                  min: 1,
-                  label: "Distance",
-                  divisions: 20,
-                  activeColor: Theme.of(context).primaryColorLight,
+        Container(
+          height: 200,
+          width: MediaQuery.of(context).size.width,
+          child: Card(
+            //add photo
+            child: Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                SingleChildScrollView(child: Image.network('https://picsum.photos/id/241/500/500',fit: BoxFit.fitWidth,)),
+                Align(
+                  alignment: Alignment.center,
+                  child: Icon(
+                      Icons.add_a_photo,
+                      size: 60,
+                      color: Colors.white,
+                    ),)
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(18, 0, 0, 0),
+                  child: Text(currentValue.toInt().toString()),
                 ),
-              ),
-              Text(maxValue.toInt().toString() + " km")
-            ],
+                Expanded(
+                  child: Slider(
+                    onChanged: (double val) {
+                      setState(() {
+                        currentValue = val;
+                      });
+                    },
+                    value: currentValue,
+                    max: maxValue,
+                    min: 0,
+                    label: currentValue.toInt().toString(),
+                    divisions: 20,
+                    activeColor: Theme.of(context).primaryColorLight,
+                  ),
+                ),
+                Text(maxValue.toInt().toString() + " km")
+              ],
+            ),
           ),
         ),
       ],
