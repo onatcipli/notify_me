517a36a26a542b88278e6847655a2b03090255f8
Mustafa Berkay <berkaymustafa03@gmail.com>
2020-01-12 16:40:51 +0300

    Create Notification - 01

--- lib/pages/create_notification_page.dart
+++ lib/pages/create_notification_page.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:notify_me/models/notification_model.dart';
+import 'package:notify_me/widgets/notification_cards/notification_card.dart';
 
 class CreateNotification extends StatefulWidget {
   @override
@@ -8,8 +10,26 @@ class CreateNotification extends StatefulWidget {
 }
 
 class _CreateNotificationState extends State<CreateNotification> {
-  double currentValue = 1;
+  int currentValue = 0;
   double maxValue = 100;
+  int _currentstep = 0;
+  void _movetonext() {
+    setState(() {
+      if (_currentstep < 2) _currentstep++;
+      else _currentstep=_currentstep;
+    });
+  }
+
+  void _movetostart() {
+    setState(() {
+      _currentstep = 0;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +37,7 @@ class _CreateNotificationState extends State<CreateNotification> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Container(
+          // Title
           padding: EdgeInsets.all(16),
           decoration: BoxDecoration(
             border: Border.all(color: Theme.of(context).primaryColorLight),
@@ -27,57 +48,72 @@ class _CreateNotificationState extends State<CreateNotification> {
             style: TextStyle(fontSize: 24),
           ),
         ),
+        NotificationCard(
+              notificationModel: NotificationModel.fromJson(
+                notifyListJson.elementAt(0),
+              )),
         Container(
-          height: 200,
-          width: MediaQuery.of(context).size.width,
-          child: Card(
-            //add photo
-            child: Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                SingleChildScrollView(child: Image.network('https://picsum.photos/id/241/500/500',fit: BoxFit.fitWidth,)),
-                Align(
-                  alignment: Alignment.center,
-                  child: Icon(
-                      Icons.add_a_photo,
-                      size: 60,
-                      color: Colors.white,
-                    ),)
-              ],
-            ),
-          ),
-        ),
-        Card(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(18, 0, 0, 0),
-                  child: Text(currentValue.toInt().toString()),
-                ),
-                Expanded(
-                  child: Slider(
-                    onChanged: (double val) {
-                      setState(() {
-                        currentValue = val;
-                      });
-                    },
-                    value: currentValue,
-                    max: maxValue,
-                    min: 0,
-                    label: currentValue.toInt().toString(),
-                    divisions: 20,
-                    activeColor: Theme.of(context).primaryColorLight,
-                  ),
-                ),
-                Text(maxValue.toInt().toString() + " km")
-              ],
+          //Stepper
+          child: Theme(
+            data: ThemeData(canvasColor: Colors.greenAccent),
+
+            child: Stepper(
+              steps: _getSteps(context),
+              type: StepperType.vertical,
+              currentStep: _currentstep,
+              onStepContinue: _movetonext,
+              onStepCancel: _movetostart,
+              // onStepTapped: _showcontent,
             ),
           ),
         ),
       ],
     );
+  }
+
+  List<Step> spr = <Step>[];
+  List<Step> _getSteps(BuildContext context) {
+    spr = <Step>[
+      Step(
+          title: Text("Kategori Seç", style: _getTextStyle(0)),
+          state: _getState(0),
+          isActive: _isActive(0),
+          content: Card()),
+      Step(
+          title: Text("Açıklama Yaz", style: _getTextStyle(1)),
+          state: _getState(1),
+          isActive: _isActive(1),
+          content: Card()),
+      Step(
+          title: Text("Fotoğraf Seç", style: _getTextStyle(2),),
+          state: _getState(2),
+          isActive: _isActive(2),
+          content: Card()),
+    ];
+    return spr;
+  }
+
+  StepState _getState(int i) {
+    if (_currentstep > i)
+      return StepState.complete;
+    else
+      return StepState.editing;
+  }
+
+  bool _isActive(int i) {
+    return _getState(i) == StepState.editing ? true : false;
+  }
+  TextStyle _getTextStyle(int i){
+    var state = _getState(i);
+     switch(state) {
+      case StepState.editing:
+        return TextStyle(color: Colors.blue);
+      case StepState.complete:
+        return TextStyle(color: Colors.blue);
+      default:
+        return TextStyle(color: Colors.red);
+
+       
+     }
   }
 }
