e937ec07d027e7dbbd69ab7509397dbc5a98495f
oguzakpinar <oguzakpinar2308@gmail.com>
2020-02-03 16:59:57 +0300

    swipe added

--- lib/widgets/notification_cards/favorite_notification_card.dart
+++ lib/widgets/notification_cards/favorite_notification_card.dart
@@ -3,7 +3,7 @@ import 'package:notify_me/models/notification_model.dart';
 import 'package:notify_me/pages/notification_stories.dart';
 import 'package:notify_me/widgets/notification_cards/vote_part.dart';
 
-class FavoriteNotificationCard extends StatelessWidget {
+class FavoriteNotificationCard extends StatefulWidget {
   final NotificationModel notificationModel;
 
   final double width;
@@ -15,143 +15,221 @@ class FavoriteNotificationCard extends StatelessWidget {
       : super(key: key);
 
   @override
+  _FavoriteNotificationCardState createState() =>
+      _FavoriteNotificationCardState();
+}
+
+class _FavoriteNotificationCardState extends State<FavoriteNotificationCard> {
+  double swipePadding = 0;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         Navigator.push(context,
             MaterialPageRoute(builder: (BuildContext context) {
           return NotificationStories(
-            notifyCardModel: notificationModel,
+            notifyCardModel: widget.notificationModel,
           );
         }));
       },
-      child: Container(
-        width: width,
-        height: height,
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          elevation: 3,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                width: width,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: NetworkImage(
-                      notificationModel.backgroundImageUrl,
+      onVerticalDragEnd: (event) {
+        double x = event.velocity.pixelsPerSecond.direction;
+        setState(() {
+          if (x < 0) {
+            swipePadding = 48;
+          } else {
+            swipePadding = 0;
+          }
+        });
+      },
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: widget.width,
+            height: widget.height,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              elevation: 3,
+              child: Container(
+                width: widget.width,
+                height: widget.height,
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.bookmark_border),
+                      color: Theme.of(context).primaryColorDark,
                     ),
-                  ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.refresh),
+                      color: Theme.of(context).primaryColorDark,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.favorite_border),
+                      color: Theme.of(context).primaryColorDark,
+                    ),
+                  ],
                 ),
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(16),
-                          bottomRight: Radius.circular(16),
-                        ),
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                          colors: <Color>[
-                            Theme.of(context).primaryColorDark,
-                            Theme.of(context).primaryColorDark,
-                            Theme.of(context).primaryColorDark.withAlpha(88),
-                            Colors.transparent,
-                          ],
-                        )),
-                    height: 200,
-                    width: 320,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Row(
-                                    children: <Widget>[
-                                      Icon(
-                                        Icons.access_time,
-                                        color:
-                                            Theme.of(context).primaryColorLight,
-                                        size: 16,
-                                      ),
-                                      SizedBox(
-                                        width: 4,
-                                      ),
-                                      Text(
-                                        notificationModel.time,
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            color: Theme.of(context)
-                                                .primaryColorLight),
-                                      )
-                                    ],
-                                  ),
-                                  Text(
-                                    notificationModel.title.length > 20
-                                        ? notificationModel.title
-                                                .substring(0, 20) +
-                                            '...'
-                                        : notificationModel.title,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    '~' + notificationModel.subTitle,
-                                    style: TextStyle(
-                                      color: Colors.white.withOpacity(0.9),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Container(
-                                width: 64,
-                                height: 64,
-                                child: CircleAvatar(
-                                  backgroundImage: NetworkImage(
-                                      notificationModel.profileImageUrl),
-                                ),
-                              ),
-                            ],
+            ),
+          ),
+          AnimatedPositioned(
+            curve: Curves.easeIn,
+            duration: Duration(milliseconds: 100),
+            bottom: swipePadding,
+            child: Container(
+              width: widget.width,
+              height: widget.height,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                elevation: 3,
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      width: widget.width,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: NetworkImage(
+                            widget.notificationModel.backgroundImageUrl,
                           ),
-                          SizedBox(height: 16),
-                          VotePart(size: 40,downColor: Colors.white70,textColor: Colors.white70,)
-                        ],
+                        ),
                       ),
                     ),
-                  ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(16),
+                                bottomRight: Radius.circular(16),
+                              ),
+                              gradient: LinearGradient(
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                                colors: <Color>[
+                                  Theme.of(context).primaryColorDark,
+                                  Theme.of(context).primaryColorDark,
+                                  Theme.of(context)
+                                      .primaryColorDark
+                                      .withAlpha(88),
+                                  Colors.transparent,
+                                ],
+                              )),
+                          height: 200,
+                          width: 320,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Row(
+                                          children: <Widget>[
+                                            Icon(
+                                              Icons.access_time,
+                                              color: Theme.of(context)
+                                                  .primaryColorLight,
+                                              size: 16,
+                                            ),
+                                            SizedBox(
+                                              width: 4,
+                                            ),
+                                            Text(
+                                              widget.notificationModel.time,
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  color: Theme.of(context)
+                                                      .primaryColorLight),
+                                            )
+                                          ],
+                                        ),
+                                        Text(
+                                          widget.notificationModel.title
+                                                      .length >
+                                                  20
+                                              ? widget.notificationModel.title
+                                                      .substring(0, 20) +
+                                                  '...'
+                                              : widget.notificationModel.title,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          '~' +
+                                              widget.notificationModel.subTitle,
+                                          style: TextStyle(
+                                            color:
+                                                Colors.white.withOpacity(0.9),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Container(
+                                      width: 64,
+                                      height: 64,
+                                      child: CircleAvatar(
+                                        backgroundImage: NetworkImage(widget
+                                            .notificationModel.profileImageUrl),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 16),
+                                VotePart(
+                                  size: 40,
+                                  downColor: Colors.white70,
+                                  textColor: Colors.white70,
+                                  downOnTap: () {
+                                    print("Badaaa");
+                                  },
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: IconButton(
+                        icon: Icon(Icons.more_vert),
+                        color: Colors.white,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Positioned(
-                top: 0,
-                right: 0,
-                child: IconButton(
-                  icon: Icon(Icons.more_vert),
-                  color: Colors.white,
-                  onPressed: () {},
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
