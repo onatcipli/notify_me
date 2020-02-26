ec1af74c81b0649ddfd97a7f321960d5f6f755f9
onatcipli <onatcipli@outlook.com>
2020-02-26 10:20:46 +0300

    removing old files for  r1

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:notify_me/pages/bottom_navigation.dart';
-import 'package:notify_me/shared/notify_light_theme.dart';
 
-void main() => runApp(NotifyMeApp());
+void main() => runApp(MyApp());
 
-class NotifyMeApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: lightTheme,
       title: "NotifyMe",
-      home: BottomNavigation(),
+      home: Scaffold(
+        body: Center(
+          child: Text('NotifyMe App'),
+        ),
+      ),
     );
   }
 }
