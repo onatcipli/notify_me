30e8d873e6eb27ea7bf2dd4cd8ce14a70ccaa55e
onatcipli <onatcipli@outlook.com>
2020-03-29 06:55:37 +0300

    android configurations done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notify_me/blocs/authentication/bloc.dart';
@@ -5,11 +6,90 @@ import 'package:notify_me/blocs/notification/bloc.dart';
 import 'package:notify_me/repositories/notification_card_repository.dart';
 import 'package:notify_me/widgets/notification_card.dart';
 import 'package:notify_me/widgets/search_bar.dart';
+
 import 'notifications.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   static final AbstractNotificationCardRepository notificationCardRepository =
       FirebaseNotificationRepository();
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  FirebaseMessaging _firebaseMessaging;
+
+  @override
+  void initState() {
+    _firebaseMessaging = FirebaseMessaging();
+    handleFirebase();
+    super.initState();
+  }
+
+  void handleFirebase() async {
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        _showItemDialog(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        //handleOnLaunch
+        _showItemDialog(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        //handleOnResume
+        _showItemDialog(message);
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(
+            sound: true, badge: true, alert: true, provisional: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      //TODO: save the token to current user document in Firestore
+    });
+  }
+
+  void _showItemDialog(Map<String, dynamic> message) {
+    showDialog<bool>(
+      context: context,
+      builder: (_) => _buildDialog(
+        context,
+        body: message['notification']['body'],
+        title: message['notification']['title'],
+      ),
+    ).then((bool shouldNavigate) {
+      if (shouldNavigate == true) {
+        //_navigateToItemDetail(message);
+      }
+    });
+  }
+
+  Widget _buildDialog(
+    BuildContext context, {
+    @required String title,
+    @required String body,
+  }) {
+    return AlertDialog(
+      title: Text(title),
+      content: Text(body),
+      actions: <Widget>[
+        FlatButton(
+          child: const Text('CLOSE'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +100,10 @@ class Home extends StatelessWidget {
           if (state is Authenticated) {
             return FloatingActionButton(
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => CreateNotification()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CreateNotification()));
               },
               child: Icon(Icons.add),
             );
